2592845681768f8a4ebad3d7c25230870a1b3e77
Maymoiz <208803754+Maymoiz@users.noreply.github.com>
2026-06-13 17:21:55 +0200

    debuged the navbar and modified it

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,31 +1,61 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./navbar.css";
+import logo from "../../assets/Moi-Tech.png";
+
 
 export default function Navbar() {
-  const [open] = useState(false);
+  const [open, setOpen] = useState(false);
+  const location = useLocation();
+
 
   return (
     <nav className="sf-nav">
-      {/* LEFT */}
+      {/* LEFT: LOGO + NAME */}
       <div className="sf-left">
         <div className="sf-logo">
+          <img src={logo} className="sf-logo-img" />
         </div>
         <span className="sf-title">Studyflow</span>
       </div>
 
-      {/* RIGHT */}
+      {/* RIGHT: NAV LINKS */}
       <div className="sf-right">
-        <div>
-  <Link to="/" className="sf-link">Dashboard</Link>
-  <Link to="/subjects" className="sf-link">Subjects</Link>
-  <Link to="/notes" className="sf-link">Notes</Link>
-  <Link to="/ai" className="sf-link">AI Tutor</Link>
-  <Link to="/progress" className="sf-link">Progress</Link>
+
+       <Link 
+  to="/" 
+  className={`sf-link ${location.pathname === "/" ? "active" : ""}`}
+>
+  Dashboard
+</Link>
+
+        <Link to="/subjects" className={`sf-link ${location.pathname === "/subjects" ? "active" : ""}`}>
+  Subjects
+</Link>
+
+       <Link to="/notes" className={`sf-link ${location.pathname === "/notes" ? "active" : ""}`}>
+  Notes
+</Link>
+
+        <Link to="/ai" className={`sf-link ${location.pathname === "/ai" ? "active" : ""}`}>
+  AI Tutor
+</Link>
+
+        <Link to="/progress" className={`sf-link ${location.pathname === "/progress" ? "active" : ""}`}>
+  Progress
+</Link>
+
+        {/* AVATAR + DROPDOWN */}
+        <div className="sf-avatar-wrapper">
+          <img
+            src="https://i.pravatar.cc/40"
+            alt="avatar"
+            className="sf-avatar"
+            onClick={() => setOpen(!open)}
+          />
+
           {open && (
             <div className="sf-dropdown">
-              <span className="sf-dropdown-item">Login</span>
-              <span className="sf-dropdown-item">Signup</span>
               <span className="sf-dropdown-item">Profile</span>
               <span className="sf-dropdown-item">Settings</span>
               <span className="sf-dropdown-item">Logout</span>
